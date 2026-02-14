5ea6ef0c0bcf891613e547ade0f56961dddff42f
ArmanSingh24 <armansingh6692@gmail.com>
2026-02-14 16:08:33 +0530

    refac(file-dest): created a new folder for images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import avatar from "@/assets/images/person-placeholder.jpg";
+
 export function Header() {
   return (
     <header className="absolute inset-x-0 top-4 max-w-full px-4 sm:px-6 lg:px-8 flex items-center justify-between" id="head">
       <div className="flex items-center gap-4">
-        <a href="index.html"
+        <a onclick={() => window.location.reload()}
           className="w-12 h-12 bg-linear-to-br from-sky-400 to-blue-600 rounded-xl flex items-center justify-center shadow-lg">
           <span className="font-bold text-white">FS</span>
         </a>
@@ -11,7 +13,7 @@ export function Header() {
         <a href="#">Docs</a>
         <a href="#">Support</a>
         <div className="w-8 h-8 rounded-full bg-linear-to-br from-green-200 to-blue-300 overflow-hidden">
-          <img src="public/person-placeholder.jpg" alt="avatar" className="w-full h-full rounded-full object-cover" />
+          <img src={avatar} alt="avatar" className="w-full h-full rounded-full object-cover" />
         </div>
       </nav>
     </header>
